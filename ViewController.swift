57af7cbc666857d79e3becf9e667c6a7a28dc2bc
Steven Guan <stevenguan@Stevens-MacBook-Air.local>
2021-03-16 18:15:34 -0700

    files

---
+++ ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  playmate
+//
+//  Created by Naomi Liu on 3/7/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var playmateLabel: UIView!
+    @IBOutlet weak var usernameTextField: UITextField!
+    @IBOutlet weak var passTextField: UITextField!
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func loginPressed(_ sender: Any) {
+    }
+    
+    @IBAction func createAccPressed(_ sender: Any) {
+    }
+}
+
